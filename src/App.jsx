9905d1b528bbddd3b8edc25ff7874e80ea5edfd0
iced-iced <109288017+iced-iced@users.noreply.github.com>
2023-01-19 20:13:38 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => {
+    return <>
+        <header>header</header>
+        <main>
+            <h1>React page</h1>
+        </main>
+        <footer>footer</footer>
+    </>
+}
